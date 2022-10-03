53f011d0284eb05af90c7ef730e178091f796f47
Karen Payne <paynekaren@comcast.net>
2022-10-03 10:28:01 -0700

    clean-up

--- IsolationWebApp/Classes/Program.cs
+++ IsolationWebApp/Classes/Program.cs
@@ -1,35 +1,8 @@
 ﻿
-using Microsoft.AspNetCore.Mvc.ApplicationModels;
-
 // ReSharper disable once CheckNamespace
 namespace IsolationWebApp;
 
 public partial class Program
 {
-    private static void Build3(WebApplicationBuilder builder)
-    {
-        builder.Services.AddRazorPages(options =>
-        {
-
-            options.Conventions.AddPageRouteModelConvention("/About", model =>
-            {
-                var selectorCount = model.Selectors.Count;
-                for (var i = 0; i < selectorCount; i++)
-                {
-                    var selector = model.Selectors[i];
-                    model.Selectors.Add(new SelectorModel
-                    {
-                        AttributeRouteModel = new AttributeRouteModel
-                        {
-                            Order = 2,
-                            Template = AttributeRouteModel.CombineTemplates(
-                                selector.AttributeRouteModel!.Template,
-                                "{aboutTemplate?}"),
-                        }
-                    });
-                }
-            });
-        });
-    }
-    
+  
 }
