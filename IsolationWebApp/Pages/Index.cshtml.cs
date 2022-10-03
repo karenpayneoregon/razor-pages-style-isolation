36f3a134b326d72aee3f39b114baa6e827d7c113
Karen Payne <paynekaren@comcast.net>
2022-10-03 13:18:59 -0700

    parms refactor

--- IsolationWebApp/Pages/Index.cshtml.cs
+++ IsolationWebApp/Pages/Index.cshtml.cs
@@ -1,8 +1,14 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using IsolationWebApp.Classes;
+using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 #pragma warning disable CS8618
 
 namespace IsolationWebApp.Pages
 {
+    /// <summary>
+    /// There is no code here pertaining to css isolation
+    /// </summary>
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
@@ -10,26 +16,21 @@ namespace IsolationWebApp.Pages
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
-
-            /*
-             * Mock-up spoken language
-             */
-            Language = "sp";
         }
 
-        public string Result { get; set; }
         /// <summary>
         /// Application spoken language
         /// </summary>
+        [FromQuery(Name = "lang")]
         public string Language { get; set; }
+
+        [FromQuery(Name = "id")]
+        public string Identifier { get; set; }
 
         public void OnGet()
         {
-
-        }
-        public void OnGetMenu1(string id1)
-        {
-            Result = "Id1: " + id1;
+            UriBuilder builder = new UriBuilder(HttpContext.Request.GetDisplayUrl());
+            Language = builder.GetLanguage();
         }
     }
 }
